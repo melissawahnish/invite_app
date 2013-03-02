62070d1bcb7735fdfd2248dae4799e64c9bc01b6
Melissa Wahnish <melissa@aarting.com>
2013-03-02 08:41:49 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InviteApp::Application.config.secret_token = 'ecd2bd7810ca82ac1eefc76897a58e88c6cda1cb69c8e5eeb4377af9bd151415c32a30f4ecd8afbad33d31f99a8a46130fad8c32617f95794c382ef10ae32eaa'
