b0204b4b85c83fc242f82d5c4a5457d3e1776031
Melissa Wahnish <melissa@aarting.com>
2013-03-03 13:47:57 -0800

    add description text field

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130302174416) do
+ActiveRecord::Schema.define(:version => 20130303213714) do
 
   create_table "events", :force => true do |t|
     t.string   "title"
     t.datetime "date"
     t.string   "location"
     t.string   "image"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+    t.text     "description"
   end
 
 end
