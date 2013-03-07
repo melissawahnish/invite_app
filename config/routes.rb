0289f0c66815343438b6d994b9aff04f8ab1db3d
Melissa Wahnish <melissa@aarting.com>
2013-03-06 20:03:49 -0800

    add authentications from scratch

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 InviteApp::Application.routes.draw do
   
+  get "sessions/new"
+
+  get "log_out" => "sessions#destroy", :as => "log_out"
+  get "log_in" => "sessions#new", :as => "log_in"
+  get "sign_up" => "users#new", :as => "sign_up"
+  root :to => "users#new"
+  resources :users
+  resources :sessions
+  
 
   resources :events
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130303213714) do
+ActiveRecord::Schema.define(:version => 20130307024018) do
 
   create_table "events", :force => true do |t|
     t.string   "title"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(:version => 20130303213714) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.text     "description"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "password_hash"
+    t.string   "password_salt"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
   end
 
 end
