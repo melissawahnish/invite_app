67b4178bc5405ef65da3ae7cb11043efb6f68f90
Melissa Wahnish <melissa@aarting.com>
2013-03-11 18:08:28 -0700

    fixes to event email

--- app/mailers/invitee_mailer.rb
+++ app/mailers/invitee_mailer.rb
@@ -3,9 +3,9 @@ class InviteeMailer < ActionMailer::Base
  
   def invitation_email(event)
    	@event = event
-    invitees = "#{@event.invitees}"
     mail(:from => "melissaecarroll@gmail.com", :to => "melissawahnish@gmail.com", 
-    	:bcc => invitees, :subject => "You've been invited!")
+    	:bcc => "#{@event.invitees}",
+    	:subject => "You've been invited!")
   end
 end
 
