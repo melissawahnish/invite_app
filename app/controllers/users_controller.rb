7e99e67ea6002084e64bdacd757bdc7530aa6d02
Melissa Wahnish <melissa@aarting.com>
2013-03-18 16:40:04 -0400

    add autologin after sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
 	def create
 	  @user = User.new(params[:user])
 	  if @user.save
+	  	session[:user_id] = @user.id
 	  	UserMailer.welcome_email(@user).deliver
 	    redirect_to root_url, :notice => "Signed up!"
 	  else
