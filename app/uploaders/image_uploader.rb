2bf40f27927f0f82c8883e1b21d16f481a4d20f2
Melissa Wahnish <melissa@aarting.com>
2013-03-27 17:25:04 -0400

    add small thumbnail to event index, make edits to event show and index pages, change home page to Event Invitations

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -46,6 +46,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     process :resize_to_limit => [200, 200]
   end
 
+  version :small_thumb do
+    process :resize_to_limit => [32, 32]
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_white_list
