1bd36c78328cf448d6681b08e9d208acaf1a3bde
Melissa Wahnish <melissa@aarting.com>
2013-04-03 21:23:57 -0400

    finish adding test to validate events

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -4,6 +4,10 @@ describe Event do
   it "has a valid factory" do
     Factory.create(:event).should be_valid
   end
-  it "is invalid without a title"
-  it "is invalid without a location"
+  it "is invalid without a title" do
+  Factory.build(:event, title: nil).should_not be_valid
+  end
+  it "is invalid without a location" do
+  Factory.build(:event, location: nil).should_not be_valid
+  end
 end
